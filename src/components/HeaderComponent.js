3ad8db207f3d0ba14aa1e7dafde9fd0463ee3616
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2022-01-31 13:21:06 -0800

    Header

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 className="App-name">Ashanti Rogers</h1>
+			</header>
+		</div>
+	);
+}
+
+export default Header;
