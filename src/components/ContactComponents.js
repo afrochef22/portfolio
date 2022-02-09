c6c2a05986cae1d37e7679e754f4b50718758aa3
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2022-02-09 09:44:39 -0800

    style contact section

--- src/components/ContactComponents.js
+++ src/components/ContactComponents.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import {
+	faTwitter,
+	faLinkedin,
+	faLinkedinIn,
+	faGithub,
+	faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Contact extends Component {
 	constructor(props) {
@@ -13,32 +21,58 @@ class Contact extends Component {
 				<div className="section-container">
 					<h1 className="section-title">Contact</h1>
 					<div className="underline"></div>
-
-					<Form inline>
-						<FormGroup>
-							<Label for="exampleEmail" hidden>
-								Email
-							</Label>
-							<Input
-								id="exampleEmail"
-								name="email"
-								placeholder="Email"
-								type="email"
-							/>
-						</FormGroup>{" "}
-						<FormGroup>
-							<Label for="examplePassword" hidden>
-								Password
-							</Label>
-							<Input
-								id="examplePassword"
-								name="password"
-								placeholder="Password"
-								type="password"
-							/>
-						</FormGroup>{" "}
-						<Button>Submit</Button>
-					</Form>
+					<div className="form-container">
+						<Form inline>
+							<FormGroup>
+								<Label for="name" hidden>
+									Name
+								</Label>
+								<Input id="name" name="name" placeholder="Name" type="name" />
+							</FormGroup>{" "}
+							<FormGroup>
+								<Label for="exampleEmail" hidden>
+									Email
+								</Label>
+								<Input
+									id="exampleEmail"
+									name="email"
+									placeholder="Email"
+									type="email"
+								/>
+							</FormGroup>{" "}
+							<FormGroup>
+								<Input
+									id="exampleText"
+									name="text"
+									type="textarea"
+									placeholder="Message"
+								/>
+							</FormGroup>
+							<Button>Submit</Button>
+						</Form>
+					</div>
+					<div className="social-container">
+						<FontAwesomeIcon
+							className="social-icon"
+							size="3x"
+							icon={faLinkedin}
+						/>
+						<FontAwesomeIcon
+							className="social-icon"
+							size="3x"
+							icon={faTwitter}
+						/>
+						<FontAwesomeIcon
+							className="social-icon"
+							size="3x"
+							icon={faGithub}
+						/>
+						<FontAwesomeIcon
+							className="social-icon"
+							size="3x"
+							icon={faInstagram}
+						/>
+					</div>
 				</div>
 			</div>
 		);
