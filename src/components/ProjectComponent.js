30be985690773a38d97fbeced920b1d6c82ac8ef
afrochef22 <70339140+afrochef22@users.noreply.github.com>
2022-02-11 13:50:38 -0800

    Add animation to project section

--- src/components/ProjectComponent.js
+++ src/components/ProjectComponent.js
@@ -15,6 +15,7 @@ import {
 } from "reactstrap";
 import pomodoro from "../images/pomodoro.png";
 import happyLife from "../images/happyLifePic2.png";
+import bgColor from "../images/bgColor.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faGithub,
@@ -26,6 +27,7 @@ import {
 	faCss3,
 	faPython,
 } from "@fortawesome/free-brands-svg-icons";
+import ScrollAnimation from "react-animate-on-scroll";
 
 class Projects extends Component {
 	constructor(props) {
@@ -51,105 +53,142 @@ class Projects extends Component {
 				<div className="section-target" id="projects"></div>
 
 				<div className="section-container tech-logo-container">
-					<h1 className="section-title third-bg">Projects</h1>
-					<div className="underline-light"></div>
+					<ScrollAnimation animateIn="fadeInDownBig">
+						<h1 className="section-title third-bg">Projects</h1>
+						<div className="underline-light"></div>
+					</ScrollAnimation>
 					<Container>
 						<Row className="cards">
 							<Col sm="">
-								<Card
-									className="project-card text-right"
-									onMouseEnter={this.toggleHover}
-									onMouseLeave={this.toggleHover}
-								>
-									{this.state.isHovered ? (
-										<React.Fragment>
-											<CardImg className="project-img" />
-											<CardImgOverlay>
-												<CardHeader>
-													<FontAwesomeIcon
-														className="card-icon"
-														size="1x"
-														icon={faCss3}
-													/>
-													<FontAwesomeIcon
-														className="card-icon"
-														size="1x"
-														icon={faJs}
-													/>
-												</CardHeader>
-												<CardBody>
-													<CardText className="card-info">
-														A pomodoro clock to help you focus.
-													</CardText>
-												</CardBody>
-												<Button
-													className="card-button"
-													href="https://my-pomodoro.herokuapp.com/"
-													target="_blank"
-												>
-													Visit
-												</Button>
-												<Button
-													className="card-button"
-													href="https://github.com/afrochef22/Pomodoro"
-													target="_blank"
-												>
-													<FontAwesomeIcon icon={faGithub} /> GitHub
-												</Button>
-											</CardImgOverlay>
-										</React.Fragment>
-									) : (
-										<CardImg
-											alt="Pomodoro image"
-											src={pomodoro}
-											className="project-img"
-										/>
-									)}
-								</Card>
+								<ScrollAnimation animateIn="fadeInRightBig">
+									<Card
+										className="project-card text-right"
+										onMouseEnter={this.toggleHover}
+										onMouseLeave={this.toggleHover}
+									>
+										{this.state.isHovered ? (
+											<React.Fragment>
+												<CardImg src={bgColor} className="project-img" />
+												<CardImgOverlay>
+													<CardHeader>
+														<ScrollAnimation
+															className="card-icon-container"
+															animateIn="bounceIn"
+														>
+															<FontAwesomeIcon
+																className="card-icon"
+																size="1x"
+																icon={faCss3}
+															/>
+														</ScrollAnimation>
+														<ScrollAnimation
+															className="card-icon-container"
+															delay={100}
+															animateIn="bounceIn"
+														>
+															<FontAwesomeIcon
+																className="card-icon"
+																size="1x"
+																icon={faJs}
+															/>
+														</ScrollAnimation>
+													</CardHeader>
+													<CardBody>
+														<CardText className="card-info">
+															A pomodoro clock to help you focus.
+														</CardText>
+													</CardBody>
+													<Button
+														className="card-button"
+														href="https://my-pomodoro.herokuapp.com/"
+														target="_blank"
+													>
+														Visit
+													</Button>
+													<Button
+														className="card-button"
+														href="https://github.com/afrochef22/Pomodoro"
+														target="_blank"
+													>
+														<FontAwesomeIcon icon={faGithub} /> GitHub
+													</Button>
+												</CardImgOverlay>
+											</React.Fragment>
+										) : (
+											<CardImg
+												alt="Pomodoro image"
+												src={pomodoro}
+												className="project-img"
+											/>
+										)}
+									</Card>
+								</ScrollAnimation>
 							</Col>
 							<Col sm="">
-								<Card
-									className="project-card"
-									onMouseEnter={this.toggleHover2}
-									onMouseLeave={this.toggleHover2}
-								>
-									{this.state.isHovered2 ? (
-										<React.Fragment>
-											<CardImg className="project-img" />
-											<CardImgOverlay>
-												<CardHeader>
-													<FontAwesomeIcon size="1x" icon={faBootstrap} />
-												</CardHeader>
-												<CardBody>
-													<CardText className="card-info">
-														An app to help discover new night life spots around
-														you.
-													</CardText>
-												</CardBody>
-												<Button
-													className="card-button"
-													href="#"
-													target="_blank"
-												>
-													Visit
-												</Button>
-												<Button
-													className="card-button"
-													href="https://github.com/afrochef22/HappyLifeReact"
-													target="_blank"
-												>
-													<FontAwesomeIcon icon={faGithub} /> GitHub
-												</Button>
-											</CardImgOverlay>
-										</React.Fragment>
-									) : (
-										<CardImg
-											alt="Pomodoro image"
-											src={happyLife}
-											className="project-img"
-										/>
-									)}
-								</Card>
+								<ScrollAnimation animateIn="fadeInLeftBig">
+									<Card
+										className="project-card"
+										onMouseEnter={this.toggleHover2}
+										onMouseLeave={this.toggleHover2}
+									>
+										{this.state.isHovered2 ? (
+											<React.Fragment>
+												<CardImg src={bgColor} className="project-img" />
+												<CardImgOverlay>
+													<CardHeader>
+														<ScrollAnimation
+															className="card-icon-container"
+															animateIn="fadeInLeft"
+														>
+															<FontAwesomeIcon
+																className="card-icon"
+																size="1x"
+																icon={faCss3}
+															/>
+														</ScrollAnimation>
+														<ScrollAnimation
+															className="card-icon-container"
+															animateIn="bounceIn"
+															delay={100}
+														>
+															<FontAwesomeIcon
+																className="card-icon"
+																size="1x"
+																icon={faBootstrap}
+															/>
+														</ScrollAnimation>
+													</CardHeader>
+													<CardBody>
+														<CardText className="card-info">
+															An app to help discover new night life spots
+															around you.
+														</CardText>
+													</CardBody>
+													<Button
+														className="card-button"
+														href="#"
+														target="_blank"
+													>
+														Visit
+													</Button>
+													<Button
+														className="card-button animate__pulse"
+														href="https://github.com/afrochef22/HappyLifeReact"
+														target="_blank"
+													>
+														<FontAwesomeIcon icon={faGithub} /> GitHub
+													</Button>
+												</CardImgOverlay>
+											</React.Fragment>
+										) : (
+											<CardImg
+												alt="Happy life image"
+												src={happyLife}
+												className="project-img"
+											/>
+										)}
+									</Card>
+								</ScrollAnimation>
 							</Col>
 						</Row>
 					</Container>
